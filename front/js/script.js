0235ec5baaa25f5922f9e34017b2fd9b952365db
juju13600 <justine.usseglio@hotmail.fr>
2022-11-06 12:23:56 +0100

    insertion de la boucle des produits

--- front/js/script.js
+++ front/js/script.js
@@ -8,3 +8,15 @@ fetch("http://localhost:3000/api/products")
     console.log("erreur lors du chargement");
   });
 
+  function getproducts(index) {
+    let zoneArticle = document.querySelector("#items");
+    for (let article of index) {
+      zoneArticle.innerHTML += `<a href="./product.html?_id=${article._id}">
+      <article>
+        <img src="${article.imageUrl}" alt="${article.altTxt}">
+        <h3 class="productName">${article.name}</h3>
+        <p class="productDescription">${article.description}</p>
+      </article>
+    </a>`;
+    }
+  }
