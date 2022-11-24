f71ad8b62c089351e9a12acb09cdd83d94f67701
juju13600 <justine.usseglio@hotmail.fr>
2022-11-24 17:15:08 +0100

    reprise de l'ajout des produits sans inner html

--- front/js/script.js
+++ front/js/script.js
@@ -1,22 +1,40 @@
 fetch("http://localhost:3000/api/products")
   .then((res) => res.json())
-  .then((product) => {
-    console.table(product);
-    getproducts(product);
+  .then((data) => {
+    console.table (data);
+    getproducts(data);
+
   })
   .catch((error) => {
     console.log("erreur lors du chargement");
   });
-
-  function getproducts(index) {
+  
+function getproducts(index) {
     let zoneArticle = document.querySelector("#items");
-    for (let article of index) {
-      zoneArticle.innerHTML += `<a href="./product.html?_id=${article._id}">
-      <article>
-        <img src="${article.imageUrl}" alt="${article.altTxt}">
-        <h3 class="productName">${article.name}</h3>
-        <p class="productDescription">${article.description}</p>
-      </article>
-    </a>`;
-    }
-  }
+   // zoneArticle.innerHTML = ""//
+    for (product of index) {
+      let link = document.createElement("a");
+      zoneArticle.appendChild(link);
+      link.href = `./product.html?id=${product._id}`;
+
+      let article = document.createElement("article");
+      link.appendChild(article);
+
+      let img = document.createElement("img");
+      article.appendChild(img);
+      img.src=product.imageUrl;
+      img.alt=product.altTxt;
+
+      let h3 = document.createElement("h3");      
+      article.appendChild(h3);
+      h3.textContent = product.name;
+
+      let p = document.createElement("p");
+      article.appendChild (p);
+      p.classList.add("productDescription");
+      p.textContent = product.description;
+      
+      console.log("affichae produit")
+    
+  };
+};
