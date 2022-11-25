164e9507b0230d9b30af1202006b2f4bb800d85c
juju13600 <justine.usseglio@hotmail.fr>
2022-11-25 16:21:23 +0100

    ajout des commentaires de la page script

--- front/js/script.js
+++ front/js/script.js
@@ -1,3 +1,4 @@
+/*Récupération des données de l'APi*/
 fetch("http://localhost:3000/api/products")
   .then((res) => res.json())
   .then((data) => {
@@ -8,33 +9,34 @@ fetch("http://localhost:3000/api/products")
   .catch((error) => {
     console.log("erreur lors du chargement");
   });
-  
+  /*Création d'une fonction pour récupérer les informations du HTML*/
 function getproducts(index) {
+  /*Création d'une boucle pour afficher les données*/
     let zoneArticle = document.querySelector("#items");
-   // zoneArticle.innerHTML = ""//
     for (product of index) {
+  /* Création du lieb vers la page produit */
       let link = document.createElement("a");
       zoneArticle.appendChild(link);
       link.href = `./product.html?id=${product._id}`;
-
+/* Création de l'article*/
       let article = document.createElement("article");
       link.appendChild(article);
-
+/*Création de l'image*/
       let img = document.createElement("img");
       article.appendChild(img);
       img.src=product.imageUrl;
       img.alt=product.altTxt;
-
+/*Création du nom du produit*/
       let h3 = document.createElement("h3");      
       article.appendChild(h3);
       h3.textContent = product.name;
-
+/*Création de la description*/
       let p = document.createElement("p");
       article.appendChild (p);
       p.classList.add("productDescription");
       p.textContent = product.description;
-      
-      console.log("affichae produit")
+/*Affichage du messae de confirmation*/
+      console.log("affichage produit")
     
   };
 };
