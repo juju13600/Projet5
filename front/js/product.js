68a3db2c0849d3db29763f1cdc4fba9ca83ba62e
juju13600 <justine.usseglio@hotmail.fr>
2022-11-26 15:55:57 +0100

    gestion du panier et des erreurs de couleurs et de quantités

--- front/js/product.js
+++ front/js/product.js
@@ -1,7 +1,8 @@
+/*Récupération de l'URL de l'Id*/
 const queryString = window.location.search
 const urlParams = new URLSearchParams(queryString)
 const id = urlParams.get("id")
-
+/*Réupération de l'article de l'API*/
 fetch("http://localhost:3000/api/products/"+id)
     .then((response) => response.json())
     .then((res) => { 
@@ -11,7 +12,7 @@ fetch("http://localhost:3000/api/products/"+id)
     .catch((error) => {
       console.log("erreur lors du chargement");
     });
-
+/*Ajout de l'image*/
 const addImage = (imageUrl, altTxt) => {
     const itemImg = document.querySelector(".item__img")
     const image = document.createElement("img")
@@ -19,18 +20,22 @@ const addImage = (imageUrl, altTxt) => {
     image.src = imageUrl
     image.alt = altTxt
 }
+/*Ajout du titre*/
 const addTitle = (name) => {
     const h1 = document.querySelector("#title")
     h1.textContent = name
 }
+/*Ajout du prix*/
 const addPrice = (price) => {
     const span = document.querySelector("#price")
     span.textContent = price
 }
+/*Ajout de la description*/
 const addDescription = (description) =>{
     const p = document.querySelector("#description")
     p.textContent = description
 }
+/*Ajout du choix des couleurs*/
 const addColors = (colors) => {
     const colorsChoice = document.querySelector("#colors")
     if (colorsChoice != null) {
@@ -42,6 +47,7 @@ const addColors = (colors) => {
         })
     }
 }
+/*Répartion des données de l'API dans le DOM */
 const detailProduct = (product) => {
   const { altTxt, colors, description, imageUrl, name, price } = product
   itemPrice = price
@@ -54,44 +60,26 @@ const detailProduct = (product) => {
   addDescription(description)
   addColors(colors)
 }
-
+/*Vérification de la couleur et de la quantité en cas d'erreur*/
 function fail(color, quantity) {
-  if (color === "" ) {
-      alert("Choisissez une couleur SVP")
-      return true
-  } 
-  if (quantity == 0 || quantity > 100 ) {
-      alert("Saisissez une quantitée inférieur à 100")
-      return true
-  } 
-}
-
-const button = document.querySelector("#addToCart")
-const addClick = () => {
-    const color = document.querySelector("#colors").value
-    const quantity = document.querySelector("#quantity").value
-    
-    if (fail(color, quantity)) return
-    saveProduct(color, quantity)
-    confirmation()
-}
-button.addEventListener("click", addClick)
-
-const saveProduct = (color, quantity) => {
-    const key = `${id}-${color}`
-    const data = {
-        id: key,
-        color: color,
-        quantity: Number(quantity),
-        price: itemPrice,
-        imageUrl: imgUrl,
-        altTxt: altText,
-        name: articleName
-    }
-    localStorage.setItem(key, JSON.stringify(data))
-}
-
-const confirmation = () => {
-    window.location.href = "cart.html"
-}
-
+    if (color === "" ) {
+        alert("Choisissez une couleur SVP")
+        return true
+    } 
+    if (quantity == 0 || quantity > 100 ) {
+        alert("Saisissez une quantitée inférieur à 100")
+        return true
+    } 
+  }
+  /*Gestion des évènements sur le bouton panier*/
+  const button = document.querySelector("#addToCart")
+  const addClick = () => {
+      const color = document.querySelector("#colors").value
+      const quantity = document.querySelector("#quantity").value
+      
+      if (fail(color, quantity)) return
+      saveProduct(color, quantity)
+      confirmation()
+  }
+  button.addEventListener("click", addClick)
+  
